63d02126e983f019a0a156ade0b9b45ee93986b7
rrogers <ryan.rogers@aitsl.edu.au>
2019-10-04 16:06:29 +1000

    new commit

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse, HttpClient } from '@angular/common/http'
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  private URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  private FORECAST = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=';
+  private KEY = '<YOUR_WEATHER_API_KEY_HERE>';
+  private IMP = '&units=imperial';
+
+  constructor(private http: HttpClient ) {}
+
+
+  searchWeatherData(cityName: string): Observable<any> {
+    return this
+              .http
+              .get(`${this.URL}${cityName}&APPID=${this.KEY}${this.IMP}`)
+              .pipe(
+               
+                tap(data => console.log(JSON.stringify(data))),
+                catchError(this.handleError)
+              );
+  }
+
+
+  private handleError(res: HttpErrorResponse) {
+    console.error(res);
+    return throwError(res.error || 'Server error');
+  }
+
+
 }
